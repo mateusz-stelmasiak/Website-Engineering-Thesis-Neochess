98fc7a51b1b694c638a9b7af5901399a6a59d873
mateusz-stelmasiak <46268673+mateusz-stelmasiak@users.noreply.github.com>
2022-01-23 18:52:09 +0100

    Added Email template

--- backEnd/Mailing.py
+++ backEnd/Mailing.py
@@ -39,16 +39,72 @@ class Mailing:
             f"Podany adres email został użyty podczas zakładania nowego konta dla użytkownika: {username}"
         )
         msg.add_alternative("""\
-            <html>
-            <head></head>
-                <body>
-                    <p>
-                        <h1>Zarejestrowałeś się na NeoChess.</h1><br>
-                        W celu potwierdzenia konta udaj się w poniższy link
-                        <p><a href=""" + confirmation_url + """>""" + confirmation_url + """</a></p>
-                    </p>
-                </body>
-            </html>
+        <html lang="pl-PL">
+<head>
+    <meta charset="utf-8"/>
+</head>
+
+<body>
+<div style="background-color:#f8f8f8; height: 30"></div>
+<div style="margin:0;padding:0!important;background-color:#f8f8f8" width="100%">
+    <center style="width:100%">
+        <div style="max-width:600px;margin:0 auto">
+            <font face="Verdana">
+                <table width="600" border="0" cellpadding="0" cellspacing="0" bgcolor="#ffffff"
+                       style="background-color: #1d1d1d; max-width:600px;width:100%;margin:auto;border-spacing:0!important;border-collapse:collapse!important;">
+
+                    <tbody>
+                    <tr>
+                        <td align="right" valign="center"
+                            style="color:#dee3e6;font-weight: bold; font-size:1.3rem; padding:10px 20px;background: linear-gradient(45deg, #b058c5 15%, #743eac 100%); height:100px;
+                                    ">
+                            <span style="filter: drop-shadow(0 0 0.2rem #dee3e6);">NeoChess</span>
+                        </td>
+                    </tr>
+                    <td height="10">&nbsp;</td>
+                    <tr>
+                        <td valign="top" style="padding: 10px 20px 0px 20px;">
+                            <h1 style="color:#97bbe8; font-size: 17pt;margin-bottom: 0px;">Welcome to NeoChess!</h1>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="left" valign="top"
+                            style="padding:0px 20px;  text-align: justify; text-justify: inter-word;color:#dee3e6">
+                            Thank you for registering on NeoChess. Before we get started, we just need to confirm that this is you.
+                            Click below to verify your email address.
+                        </td>
+                    </tr>
+                    <td height="30px">&nbsp;</td>
+
+                    <tr>
+                        <td align="center" valign="top" style="padding:20px 0">
+                            <h2 style="color:#97bbe8">Click to confirm your email</h2>
+                            <a href="""+confirmation_url+"""
+                               style=" background-color: #743eac;border-radius: 10px;color: white; padding: 15px 40px;text-align: center; text-decoration: none; display: inline-block; margin: 4px 2px;cursor: pointer">
+                                <b>Confirm email</b>
+                            </a>
+                        </td>
+                    </tr>
+
+
+                    <td height="250">&nbsp;</td>
+                    <tr>
+                        <td align="center" valign="top" style="padding:20px 0;background-color:#191919;color:#dee3e6;font-size:0.9rem ">
+                           This message was sent by <br> <b>NeoChess</b>
+                        </td>
+                    </tr>
+
+                    </tbody>
+                </table>
+            </font>
+        </div>
+</div>
+</center>
+</div>
+<div style="background-color:#f8f8f8; height: 30"></div>
+</body>
+
+</html>
             """.format(cid=cid[1:-1]), subtype='html')
 
         self.__send_email(receiver_mail, msg)
