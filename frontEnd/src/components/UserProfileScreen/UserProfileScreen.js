85ffc3a826f33f17a21d17a962a5ae0f5d97e813
karolkarolski25 <karolkarolski25@gmail.com>
2022-01-24 23:59:31 +0100

    removed empty lines

--- frontEnd/src/components/UserProfileScreen/UserProfileScreen.js
+++ frontEnd/src/components/UserProfileScreen/UserProfileScreen.js
@@ -60,26 +60,20 @@ function UserProfileScreen(props) {
         'color': 'var(--text-color)',
     }
 
-
     return (
         <FooterHeaderWithMarginsLayout className="UserProfileScreen">
             <SectionTitle>MANAGE ACCOUNT</SectionTitle>
-
             <Section id="UserProfileScreen">
                 <div style={sectionStyle} className="CurrentInfo-container">
-
                     <div style={containerStyle}>
                         <h1 style={glowingStyle}>CURRENT INFO</h1>
                         <span className="container"><h3>Username</h3><span>{props.username}</span></span>
                         <span className="container"><h3>Acc created</h3><span><Dots>loading</Dots></span></span>
                         <span className="container"><h3>Email address</h3><span><Dots>loading</Dots></span></span>
                     </div>
-
                     <FenDisplayingBoard/>
                 </div>
-
             </Section>
-
             <Section id="UserProfileEdition">
                 {are_fields_correct() ?
                     <UserEditForm
@@ -87,10 +81,7 @@ function UserProfileScreen(props) {
                         email={email}
                         is2FaEnabled={is2FaEnabled}
                     /> : null}
-
             </Section>
-
-
         </FooterHeaderWithMarginsLayout>
     );
 }
