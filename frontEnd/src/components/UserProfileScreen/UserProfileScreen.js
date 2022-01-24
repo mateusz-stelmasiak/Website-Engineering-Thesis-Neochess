7c55cfb1e9e06243a490777aa6074d57717994fc
mateusz-stelmasiak <46268673+mateusz-stelmasiak@users.noreply.github.com>
2022-01-25 00:56:06 +0100

    Restore useProfile changes

--- frontEnd/src/components/UserProfileScreen/UserProfileScreen.js
+++ frontEnd/src/components/UserProfileScreen/UserProfileScreen.js
@@ -15,19 +15,27 @@ function UserProfileScreen(props) {
     const [email, setEmail] = useState(undefined);
     const [is2FaEnabled, setIs2FaEnabled] = useState(undefined);
     const [accCreated, setAccCreated] = useState(undefined);
+    const [accountCreatedTime, setAccountCreatedTime] = useState(undefined);
+    const [accountUpdatedTime, setAccountUpdatedTime] = useState(undefined);
 
     useEffect(async () => {
         const response = (await getUserData())
+        console.log(response)
         setEmail(response['Email'])
         setIs2FaEnabled(response['2FA'])
         setAccCreated(response['Joined'])
+        setAccountUpdatedTime(response['UpdatedAt'])
+        setAccountCreatedTime(response['CreatedAt'])
+
     }, [])
 
     const are_fields_correct = () => {
         return props.username !== undefined &&
             email !== undefined &&
             is2FaEnabled !== undefined &&
-            accCreated !== undefined
+            accCreated !== undefined&&
+            accountCreatedTime !==undefined &&
+            accountUpdatedTime !==undefined
     }
 
     let glowingStyle = {
@@ -71,8 +79,12 @@ function UserProfileScreen(props) {
                             <span>{are_fields_correct() ? props.username: <Dots>loading</Dots>}</span>
                         </span>
                         <span className="container">
-                            <h3>Acc created</h3>
-                            <span>{are_fields_correct() ? accCreated: <Dots>loading</Dots>}</span>
+                            <h3>Account created</h3>
+                            <span>{are_fields_correct() ? accountCreatedTime: <Dots>loading</Dots>}</span>
+                        </span>
+                        <span className="container">
+                            <h3>Account updated</h3>
+                            <span>{are_fields_correct() ? accountUpdatedTime: <Dots>loading</Dots>}</span>
                         </span>
                         <span className="container">
                             <h3>Email address</h3>
