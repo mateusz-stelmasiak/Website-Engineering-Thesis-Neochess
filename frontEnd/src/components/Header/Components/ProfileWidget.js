ca696b9c9cc8eb51d270c23d946d8eaac0639e79
mateusz-stelmasiak <46268673+mateusz-stelmasiak@users.noreply.github.com>
2022-01-25 08:36:37 +0100

    Logo update

--- frontEnd/src/components/Header/Components/ProfileWidget.js
+++ frontEnd/src/components/Header/Components/ProfileWidget.js
@@ -3,28 +3,32 @@ import "./ProfileWidget.css";
 import {connect} from "react-redux";
 import {mapAllStateToProps} from "../../../redux/reducers/rootReducer";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faSignOutAlt,faCog} from "@fortawesome/free-solid-svg-icons";
+import {faSignOutAlt, faCog} from "@fortawesome/free-solid-svg-icons";
 
 import {logout} from "../../../serverCommunication/LogRegService";
 import {useHistory} from "react-router-dom";
+import logo from '../../../assets/neochess-logo.png'; // Tell webpack this JS file uses this image
 
 
-function ProfileWidget(props){
-        let logoutIcon=<FontAwesomeIcon
-            icon={faSignOutAlt}
-            onClick={logout}
-        />
+function ProfileWidget(props) {
+    let logoutIcon = <FontAwesomeIcon
+        icon={faSignOutAlt}
+        onClick={logout}
+    />
 
-        //route to configure page after clicking configure icon
-        const history = useHistory();
-        const routeToConfigure = () => history.push('/profile');
-        let configureIcon=<FontAwesomeIcon
-            icon={faCog}
-            onClick={routeToConfigure}
-        />
+    //route to configure page after clicking configure icon
+    const history = useHistory();
+    const routeToConfigure = () => history.push('/profile');
+    const routeToMain = () => history.push('/');
+    let configureIcon = <FontAwesomeIcon
+        icon={faCog}
+        onClick={routeToConfigure}
+    />
 
-        return (
-            <div className="ProfileWidget">
+    return (
+        <div className="ProfileWidget">
+            <div className="ProfileWidget-container">
+                <img src={logo} onClick={routeToMain} alt="Logo"/>
                 <div className="ProfileWidget-info">
                     <h1>{props.username}</h1>
                     <div className="ProfileWidget-info-container">
@@ -33,10 +37,13 @@ function ProfileWidget(props){
                         <span>{logoutIcon}</span>
                     </div>
                 </div>
+
             </div>
-        );
+        </div>
+    );
 
 }
+
 const mapStateToProps = (state) => {
     return {
         username: state.user.username,
